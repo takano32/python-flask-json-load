b354dcec72b259c8b1860a8a1578dc29c1e0d1e9
TAKANO Mitsuhiro <takano32@gmail.com>
2022-08-15 11:23:04 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return jsonify(request.json)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
